62eeddc4af51bee2ffc004cf4fed72fd6ab1e8c4
Fabio Martins <fabio@inventae.com.br>
2015-08-07 11:51:34 -0300

    Rotas alteradas

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth', format: :json
+  mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
 
 	resources :clients
   resources :street_suffixes
